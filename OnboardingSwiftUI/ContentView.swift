55daa809a6f969cdab5eeebccb9049477c47a3b6
Wingovers <ryanpferrell@gmail.com>
2020-06-20 18:25:09 -0700

    Initial Commit

---
+++ OnboardingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingSwiftUI
+//
+//  Created by Ryan on 6/20/20.
+//  Copyright © 2020 Wingover. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
